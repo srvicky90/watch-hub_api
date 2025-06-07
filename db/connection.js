f28d71e87a286e6507ad57ebdcd1e8999323041b
srvicky90 <srvicky90@gmail.com>
2025-06-07 19:02:13 +0530

    updated the password for the mongodb

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://srvignesh:MyMongo$09@cluster0.ofqeznu.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://srvignesh:MyMongodb$09@cluster0.ofqeznu.mongodb.net/?retryWrites=true&w=majority')
     .then(() => console.log('Successfully connected to the database. You are all set to go!!! '))
     .catch(err => console.error('Something went wrong', err));
